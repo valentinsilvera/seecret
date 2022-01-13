329eb4af82fb8d83f495bd5e1ccc0edccd609a77
Valentin <aplbep@outlook.com>
2022-01-12 23:42:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const typeDefs = gql`
+    type Query {
+        sayHi: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello World!'
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+mongoose.connect
+
+server.listen({port : 8000}).then((res) => {
+    console.log(`Server running at $(res.url)`);
+})
