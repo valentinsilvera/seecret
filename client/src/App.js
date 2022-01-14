bc906f33f9eafcdbdfa3a063320a1c683b28bd2d
Valentin <aplbep@outlook.com>
2022-01-14 17:29:02 -0300

    created client

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <h1>
+                Seecret
+                <span role="img" aria-label="shhh">
+                    🤫
+                </span>
+            </h1>
+        </div>
+    );
+}
+
+export default App;
