3475e48c9bb2611fa33faff1b5c917d235a3ce50
Valentin <aplbep@outlook.com>
2022-01-15 10:40:13 -0300

    connected local server to client

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
 export default function Home() {
+    const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+    if (data) {
+        console.log(data);
+    }
     return (
         <div>
             <h1>Home Page</h1>
         </div>
     );
 }
+
+const FETCH_POSTS_QUERY = gql`
+    {
+        getPosts {
+            id
+            body
+            createdAt
+            username
+            likes {
+                username
+            }
+            comments {
+                id
+                username
+                createdAt
+                body
+            }
+        }
+    }
+`;
