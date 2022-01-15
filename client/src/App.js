027fdf19c50f5a5f83a28a9acd0b4264da59d073
Valentin <aplbep@outlook.com>
2022-01-15 01:03:29 -0300

    created and connected react app, added semantic ui and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
     return (
-        <div>
-            <h1>
-                Seecret
-                <span role="img" aria-label="shhh">
-                    🤫
-                </span>
-            </h1>
-        </div>
+        <Router>
+            <Container>
+                <MenuBar />
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/login" element={<Login />} />
+                    <Route exact path="/register" element={<Register />} />
+                </Routes>
+            </Container>
+        </Router>
     );
 }
 
