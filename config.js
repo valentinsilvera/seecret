6c8401f4d10e02111a131d0cc46a5c33533859b0
Valentin <aplbep@outlook.com>
2022-01-17 02:19:42 -0300

    added secrets

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB:
+        'mongodb+srv://valentin:QW2ykt2wFzFcKYk5N4ar@cluster0.vziwf.mongodb.net/seecret?retryWrites=true&w=majority',
+    SECRET_KEY: 'OJdXAqNRqjevSvcWUHDeMNpO7OrHGLH1',
+};
