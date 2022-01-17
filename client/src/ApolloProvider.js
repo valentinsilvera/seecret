7cd6cd3fc261473d29a69c95da5ab6aa223f5f9e
Valentin <aplbep@outlook.com>
2022-01-17 07:36:10 -0300

    edited proxy and server base url

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:8000',
+    uri: 'https://seecret.herokuapp.com',
 });
 
 const authLink = setContext(() => {
